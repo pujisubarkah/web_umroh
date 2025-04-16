99b106b8c942413824a2628f98755f2fc54f7ed9
Rezky Yayang Yakhamid <60925883+rezkyyayang@users.noreply.github.com>
2025-04-16 14:33:44 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Navbar from '@/components/Navbar';
 import Carousel from '@/components/Carousel';
+import Footer from '@/components/Footer';
 
 export default function Home() {
   return (
@@ -13,6 +14,11 @@ export default function Home() {
         <h1 className="text-3xl font-bold">Selamat datang di Homepage!</h1>
         <p className="mt-2 text-gray-600">Ini konten homepage kamu, gaskeun kustomisasi 🎨</p>
       </section>
+
+      <div className="mt-4">
+        <Footer />
+      </div>
+
     </main>
   );
 }
