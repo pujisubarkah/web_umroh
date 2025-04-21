a7378f0eefb1283cea2473201201259f5840489f
Rezky Yayang Yakhamid <60925883+rezkyyayang@users.noreply.github.com>
2025-04-21 22:51:06 +0700

    Update page.tsx

--- src/app/umroh/reguler/page.tsx
+++ src/app/umroh/reguler/page.tsx
@@ -1,1 +1,31 @@
+import Carousel from '@/components/Carousel';
+import Footer from '@/components/Footer';
+import Navbar from '@/components/Navbar'; // Jangan lupa Navbar juga harus diimport
+import Chatbot from '@/components/Chatbot'; // Impor komponen Chatbot
+import Translator from '@/components/Translator'; // Impor komponen Translator
 
+export default function Home() {
+  return (
+    <main className="relative">
+      <div className="sticky top-0 z-50">
+        <Navbar />
+      </div>
+      <div className="mt-4">
+        <Carousel />
+      </div>
+      <section className="p-8">
+        {/* Konten lainnya */}
+      </section>
+      <div className="mt-4">
+        <Footer />
+      </div>
+
+      {/* Tambahkan komponen Chatbot di sini */}
+      <div className="fixed bottom-0 right-0 m-4 sticky top-0 z-100">
+        <Chatbot />
+      </div>
+      {/* Tambahkan komponen Translator di sini */}
+      <Translator />
+    </main>
+  );
+}
