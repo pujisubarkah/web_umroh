338354bb7a83f11419b9b6bac74c48ea42d3be4b
pujisubarkah <pujiatmo.subarkah@gmail.com>
2025-04-21 09:36:06 +0700

    berubah dikit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -63,6 +63,10 @@ const navItems = [
 declare global {
   interface Window {
     gtranslateSettings?: {
+      default_language?: string;
+      native_language_names?: boolean;
+      languages?: string[];
+      wrapper_selector?: string;
       translatePage?: (lang: string) => void;
     };
   }
